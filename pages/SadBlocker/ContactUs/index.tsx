4cd04ba0e2b9b7cc3a978fbba0bba48e51772eb3
ashraf <ashrafainia1995@gmail.com>
2023-12-03 23:52:50 +0300

    feat: SAD Blocker pages

---
+++ pages/SadBlocker/ContactUs/index.tsx
@@ -0,0 +1,93 @@
+import Head from "next/head";
+import React from "react";
+
+import styles from "./index.module.css";
+
+const ContactUs = () => {
+  const [formData, setFormData] = React.useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e: any) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const generateMailtoLink = () => {
+    const { name, email, message } = formData;
+    const subject = "New Contact Us Submission";
+    const body = `Name: ${name}%0AEmail: ${email}%0AMessage: ${message}`;
+    return `mailto:meshal.albshry@gmail.com?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(body)}`;
+  };
+
+  return (
+    <>
+      <Head>
+        <title>اتصل بنا</title>
+      </Head>
+      <div className={styles.container}>
+        <h1 className={styles.heading}>اتصل بنا</h1>
+
+        <p>نحن هنا لمساعدتك. يرجى ملء النموذج أدناه للتواصل معنا.</p>
+
+        <form className={styles.form}>
+          <label className={styles.label} htmlFor="name">
+            الاسم الكامل:
+          </label>
+          <input
+            className={styles.inputField}
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+
+          <label className={styles.label} htmlFor="email">
+            البريد الإلكتروني:
+          </label>
+          <input
+            className={styles.inputField}
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+
+          <label className={styles.label} htmlFor="message">
+            رسالتك:
+          </label>
+          <textarea
+            className={styles.inputField}
+            id="message"
+            name="message"
+            rows={6}
+            value={formData.message}
+            onChange={handleChange}
+            required
+          ></textarea>
+
+          <a
+            className={styles.submitButton}
+            href={generateMailtoLink()}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            إرسال
+          </a>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default ContactUs;
---
+++ pages/SadBlocker/Privacy.tsx
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+
+const Privacy = () => {
+  return (
+    <div>
+      <Head>
+        <title>سياسة الخصوصية</title>
+      </Head>
+      <div style={{ textAlign: "center", direction: "rtl" }}>
+        <h1>سياسة الخصوصية</h1>
+
+        <p>
+          نحن نهتم بخصوصيتك، ونرغب في أن تكون متأكدًا من أنك تفهم كيف نقوم بجمع
+          واستخدام وحماية معلوماتك.
+        </p>
+
+        <h2>عدم حفظ بيانات المستخدم</h2>
+
+        <p>
+          نود أن نؤكد لك أننا لا نقوم بحفظ أي من معلومات المستخدم الخاصة بك. نحن
+          نحترم خصوصيتك ونلتزم بتوفير تجربة استخدام آمنة وخصوصية.
+        </p>
+
+        <p>
+          إذا كان لديك أي أسئلة إضافية أو استفسارات حول سياسة الخصوصية، يرجى .
+          <Link href="/SadBlocker/Support" style={{ color: "blue" }}>
+            الاتصال بنا
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Privacy;
